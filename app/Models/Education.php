233c7d89ae6659ef70d71a321c004654a5108402
amitleuva1987 <amitleuva1987@gmail.com>
2022-08-05 11:06:32 +0530

    Created DB models for all migrations

---
+++ app/Models/Education.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Education extends Model
+{
+    use HasFactory;
+    protected $fillable = ['degree_name','university_name','from_date','to_date'];
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'mobile_no', 'job_title', 'location'
     ];
 
     /**
@@ -37,4 +37,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function user_details(){
+        return $this->hasOne(UserDetails::class);
+    }
 }
