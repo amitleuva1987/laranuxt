2696d8d5e994d6c11e3472cfd3b07f8255d046ea
amitleuva1987 <amitleuva1987@gmail.com>
2022-08-05 11:13:13 +0530

    Added and later removed header from the default layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,6 +1,15 @@
 <template>
-  <div class="flex items-center justify-center">
-    <Nuxt />
+  <div>
+    <!-- <nav class="p-5 flex flex-row bg-red-400">
+        <div class="md:w-1/2">Brand</div>
+        <div class="md:w-1/2 flex justify-end space-x-5">
+            <a href="#">Sign up</a>
+            <a href="#">Login</a>
+        </div>
+  </nav> -->
+    <div class="flex items-center justify-center">
+      <Nuxt />
+    </div>
   </div>
 </template>
 
