209da2248f70334b7a8c245ddd815d0e3caad3b9
amitleuva1987 <amitleuva1987@gmail.com>
2022-08-05 11:32:59 +0530

    Created Edit Skill component

---
+++ client/components/resume/EditSkill.vue
@@ -0,0 +1,61 @@
+<template>
+  <form class="w-full">
+    <div class="flex items-center border-b border-teal-500 py-2">
+      <input v-model="skill.name" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Skill">
+      <button class="bg-blue-500 hover:bg-blue-700 border-blue-500 hover:text-md border-4 text-white py-2 px-3 rounded" type="button" @click.once="edit_skill">
+        SAVE
+      </button>
+      <button class="border-white bg-white border-4 text-teal-500 hover:text-teal-800 text-md py-2 px-2 rounded" type="button" @click="toggleEditSkill">
+        Cancel
+      </button>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Skill } from '@/types/api'
+export default Vue.extend({
+  name: 'EditSkill',
+  props: ['id'],
+  data () {
+    const skill:Skill = {
+      name: '',
+    }
+    return {
+      skill,
+    }
+  },
+  async mounted () {
+    const url = 'skills/' + this.id
+    this.skill = (await this.$axios.get(url)).data.data as Skill
+  },
+  methods: {
+    async edit_skill ():Promise<void> {
+      const data = {
+        name: this.skill.name,
+      }
+      const url = 'skills/' + this.id
+      await this.$axios.put(url, data).then((response) => {
+        this.$toast.show({
+          type: 'success',
+          title: 'Success',
+          message: 'Skill Updated Successfully',
+        })
+        this.toggleEditSkill()
+        this.$accessor.skill.updateSkills()
+      }).catch((error) => {
+        this.$toast.show({
+          type: 'danger',
+          title: 'Error',
+          message: 'Oops, something went wrong',
+        })
+      })
+    },
+
+    toggleEditSkill ():void {
+      this.$accessor.skill.setEditSkill()
+    },
+  },
+})
+</script>
