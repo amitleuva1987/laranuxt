2daa0a1129079eebbda97ed2aa544f9082bd72d3
amitleuva1987 <amitleuva1987@gmail.com>
2022-08-05 11:23:03 +0530

    Created User Test

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $user = User::factory()->create(1);
+        $response = $this->get('/users/'.$user->id);
+        $response->assertStatus(200);
+    }
+}
