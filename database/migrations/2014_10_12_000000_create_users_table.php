b68548785baafd16698f9c8a35806f019f2a0479
amitleuva1987 <amitleuva1987@gmail.com>
2022-08-05 11:00:34 +0530

    Created Migrations for education, skills, experience, hobby, language

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,14 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('job_title');
+            $table->string('mobile_no');
+            $table->string('github_profile')->nullable();
+            $table->string('linkedin_profile')->nullable();
+            $table->string('location');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('description')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
